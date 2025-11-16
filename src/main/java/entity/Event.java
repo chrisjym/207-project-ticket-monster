9fa40d0243768bc71327b200fcb0c88b084f6082
Joyi <joyi.x.06@gmail.com>
2025-11-15 22:54:36 -0500

    location entity initialized
    also updated event entity to update change in location

--- src/main/java/entity/Event.java
+++ src/main/java/entity/Event.java
@@ -5,47 +5,27 @@ public class Event {
     private final String name;
     private final String description;
     private final String address;
-    private final double latitude;
-    private final double longitude;
+    private final Location location;
     private final String category;
-    private final String dateTime; // or LocalDateTime
+    private final String dateTime;
 
     public Event(String id, String name, String description,
-                 String address, double latitude, double longitude,
+                 String address, Location location,
                  String category, String dateTime) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
+        this.location = location;
         this.category = category;
         this.dateTime = dateTime;
     }
 
-    public String getName() {
-        return name;
-    }
-    public String getDescription() {
-        return description;
-    }
-
-    public String getId() {
-        return id;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public double getLatitude() {
-        return latitude;
-    }
-    public double getLongitude() {
-        return longitude;
-    }
-    public String getCategory() {
-        return category;
-    }
-    public String getDateTime() {
-        return dateTime;
-    }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public String getId() { return id; }
+    public String getAddress() { return address; }
+    public Location getLocation() { return location; }
+    public String getCategory() { return category; }
+    public String getDateTime() { return dateTime; }
 }
